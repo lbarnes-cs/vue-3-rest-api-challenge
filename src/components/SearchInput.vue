34363f1ee27f67958dabb70832dc165e2463cd87
Laurence Barnes <laurence.melb@gmail.com>
2025-04-13 15:39:46 +0200

    fix UI issue with chrome

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -7,7 +7,8 @@
     density="compact"
     variant="outlined"
     hide-details
-    @click:append-inner="onClick"
+    @click:append-inner="handleAppendClick"
+    @update:modelValue="debouncedEmit"
   />
 </template>
 
@@ -26,6 +27,15 @@ const emit = defineEmits<{
 const localSearchKey = ref('');
 const isClickTriggered = ref(false); // Track if click was triggered
 
+/**
+ * TODO:
+ * The user experience here is messy and needs to be improved.
+ * In an effort to use debounced, ensuring we don't send API requests for every
+ * key-change on the input; we have an issue where the value doesn't get saved correctly.
+ *
+ * Might look into using a computed function with getter and setter to better manage this
+ * For the challenge, this is low on the priority list.
+ */
 // Debounced function to emit the search term
 const debouncedEmit = useDebounceFn((value: string) => {
   // Ensure we don't emit twice if the user already press the search icon
@@ -35,13 +45,13 @@ const debouncedEmit = useDebounceFn((value: string) => {
 }, 500);
 
 // Watch for changes in the search field and trigger debounced emit
-const watchLocalSearchKey = (newVal: string) => {
-  debouncedEmit(newVal);
-};
+// const watchLocalSearchKey = (newVal: string) => {
+//   debouncedEmit(newVal);
+// };
 
-watch(localSearchKey, watchLocalSearchKey);
+// watch(localSearchKey, watchLocalSearchKey);
 
-function onClick() {
+const handleAppendClick = () => {
   // Emit the latest value when the append-inner area is clicked
   // Stop a user from calling the API mulitple times
   if (!props.isFetching && !isClickTriggered.value) {
@@ -55,5 +65,25 @@ function onClick() {
   setTimeout(() => {
     isClickTriggered.value = false;
   }, 600); // Slightly longer than the debounce delay to avoid conflict
-}
+};
 </script>
+
+<style lang="scss" scoped>
+// Overrides Chrome autofill styles,
+// which was adding a white background when we select a suggested word,
+// destorying the UI in the process
+:deep(input) {
+  background-color: transparent !important; // fallback for normal state
+  color: inherit !important;
+}
+
+:deep(input:-webkit-autofill),
+:deep(input:-webkit-autofill:focus),
+:deep(input:-internal-autofill-selected) {
+  box-shadow: 0 0 0px 1000px transparent inset !important; // force transparent bg
+  -webkit-text-fill-color: inherit !important; // ensure text color matches theme
+  transition:
+    background-color 9999s ease-out,
+    color 9999s ease-out !important; // delay Chrome override
+}
+</style>
